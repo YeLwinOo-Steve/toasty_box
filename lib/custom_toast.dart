e9d45bb00985beec7d247d24d185e3505867679c
crazecoder <21527312@qq.com>
2025-03-12 20:26:29 +0800

    Fixed close button occlusion when text is too long

--- lib/custom_toast.dart
+++ lib/custom_toast.dart
@@ -13,6 +13,7 @@ class CustomToast extends StatefulWidget {
   final VoidCallback? onClose;
   final Curve? curve;
   final bool? isClosable;
+
   const CustomToast({
     super.key,
     this.isInFront = false,
@@ -52,67 +53,65 @@ class _CustomToastState extends State<CustomToast> {
                   reverseCurve: widget.curve ?? Curves.elasticOut,
                 ),
               ),
-              child: Stack(
-                children: [
-                  InkWell(
-                    onTap: widget.onTap,
-                    borderRadius: BorderRadius.circular(15),
-                    child: Container(
-                      width: MediaQuery.sizeOf(context).width,
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 16, vertical: 20),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: widget.backgroundColor ?? Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            blurRadius: widget.isInFront ? 0.5 : 0.0,
-                            offset: const Offset(0.0, -1.0),
-                            color: widget.shadowColor ?? Colors.grey.shade400,
-                          ),
-                          BoxShadow(
-                            blurRadius: widget.isInFront ? 12 : 3,
-                            offset: const Offset(0.0, 7.0),
-                            color: widget.shadowColor ?? Colors.grey.shade400,
-                          ),
-                        ],
-                      ),
-                      child: (widget.child != null)
-                          ? widget.child
-                          : Row(
-                              children: [
-                                if (widget.leading != null) ...[
-                                  widget.leading!,
-                                  const SizedBox(
-                                    width: 10,
-                                  ),
-                                ],
-                                if (widget.message != null)
-                                  Expanded(
-                                    child: Text(
-                                      widget.message!,
-                                      style: widget.messageStyle,
-                                    ),
-                                  ),
-                              ],
-                            ),
+              child: Container(
+                width: MediaQuery.sizeOf(context).width,
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 16, vertical: 20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: widget.backgroundColor ?? Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: widget.isInFront ? 0.5 : 0.0,
+                      offset: const Offset(0.0, -1.0),
+                      color: widget.shadowColor ?? Colors.grey.shade400,
                     ),
-                  ),
-                  if (widget.isClosable ?? false)
-                    Positioned(
-                      top: 0,
-                      right: 16,
-                      bottom: 0,
+                    BoxShadow(
+                      blurRadius: widget.isInFront ? 12 : 3,
+                      offset: const Offset(0.0, 7.0),
+                      color: widget.shadowColor ?? Colors.grey.shade400,
+                    ),
+                  ],
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Expanded(
                       child: InkWell(
+                        onTap: widget.onTap,
+                        borderRadius: BorderRadius.circular(15),
+                        child: (widget.child != null)
+                            ? widget.child
+                            : Row(
+                          children: [
+                            if (widget.leading != null) ...[
+                              widget.leading!,
+                              const SizedBox(
+                                width: 10,
+                              ),
+                            ],
+                            if (widget.message != null)
+                              Expanded(
+                                child: Text(
+                                  widget.message!,
+                                  style: widget.messageStyle,
+                                ),
+                              ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    if (widget.isClosable ?? false)
+                      InkWell(
                         onTap: widget.onClose,
                         child: const Icon(
                           Icons.close,
                           size: 18,
                         ),
-                      ),
-                    ),
-                ],
-              ),
+                      )
+                  ],
+                ),
+              ) ,
             ),
           );
         });
